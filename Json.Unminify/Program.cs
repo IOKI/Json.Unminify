27d451d2ff7aa00434c8156ac7e2fea845bdaca7
Jakub Gruszecki <jakub.gruszecki@pearson.com>
2015-10-30 12:07:45 +0100

    Unminifying json.

---
+++ Json.Unminify/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Json.Unminify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var fileStream = File.OpenRead(args[0]))
+            using (var stremReader = new StreamReader(fileStream))
+            using (var jsonReader = new JsonTextReader(stremReader))
+            {
+                var json = JObject.Load(jsonReader);
+                File.WriteAllText(args[1], json.ToString(Formatting.Indented));
+            }
+        }
+    }
+}
